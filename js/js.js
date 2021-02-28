2531165df911176b9a803e1146fb73b113685bd3
Ilya Shevchyk <il2001sh@gmail.com>
2021-02-28 16:57:58 +0200

    dumb function

--- js/js.js
+++ js/js.js
@@ -1,20 +1,24 @@
 
 
-function add() {
-    let element = document.getElementById('add');
-    let addedItem = document.createElement('span');
-    addedItem.innerText = 'ADDED SOME TEXT';
-    element.append(addedItem);
-}
+// function add() {
+//     let element = document.getElementById('add');
+//     let addedItem = document.createElement('span');
+//     addedItem.innerText = 'ADDED SOME TEXT';
+//     element.append(addedItem);
+// }
 
-function remove(){
-    let element = document.querySelector('#add > span');
-    element.remove();
-}
+// function remove(){
+//     let element = document.querySelector('#add > span');
+//     element.remove();
+// }
 
-function insert(){
-    let element = document.getElementById('add');
-    let newItem = document.createElement('span');
-    newItem.innerText = 'ADDED SOME OTHER TEXT';
-    element.after(newItem);
+// function insert(){
+//     let element = document.getElementById('add');
+//     let newItem = document.createElement('span');
+//     newItem.innerText = 'ADDED SOME OTHER TEXT';
+//     element.after(newItem);
+// }
+
+function doSomething(){
+    document.getElementById("noSharing").value = "I`m going to share this text";
 }
