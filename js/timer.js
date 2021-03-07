9aa6ecabd76cf3b019b89b5084f4ab6baadf7e24
Ilya Shevchyk <il2001sh@gmail.com>
2021-03-07 19:49:22 +0200

    made DOOM

--- js/timer.js
+++ js/timer.js
@@ -1,7 +1,10 @@
+
 function doom() {
     let timer = createTimer();
     document.getElementById('doomsDay').appendChild(timer);
     document.getElementById('start').disabled = true;
+    let intID = setInterval(() => makeDoom(intID), 1000);
+    
 }
 
 function createTimer() {
@@ -12,4 +15,12 @@ function createTimer() {
     time.setAttribute('step', '2');
     time.setAttribute('value', '0');
     return time;
+}
+
+function makeDoom(intervalTimeout) {
+    let timerObj = document.querySelector('#doomsDay > input');
+    timerObj.value = 5 + Number(timerObj.value);
+    if(timerObj.value == timerObj.max){
+        clearInterval(intervalTimeout);
+    }
 }
