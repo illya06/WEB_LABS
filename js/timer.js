410895da643276df6ec5ddda072450e538821b7a
Ilya Shevchyk <il2001sh@gmail.com>
2021-03-07 19:22:02 +0200

    made doom inevitable

--- js/timer.js
+++ js/timer.js
@@ -1,13 +1,15 @@
-function doom(){
+function doom() {
     let timer = createTimer();
     document.getElementById('doomsDay').appendChild(timer);
+    document.getElementById('start').disabled = true;
 }
 
-function createTimer(){
+function createTimer() {
     let time = document.createElement('input');
     time.setAttribute('type', 'range');
     time.setAttribute('max', '100');
     time.setAttribute('min', '0');
     time.setAttribute('step', '2');
+    time.setAttribute('value', '0');
     return time;
 }
