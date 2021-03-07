4f4b8edb124daf16fdb1cb6e928c478f4e239e35
Ilya Shevchyk <il2001sh@gmail.com>
2021-03-07 19:13:55 +0200

    added timer creation func

---
+++ js/timer.js
@@ -0,0 +1,12 @@
+function doom(){
+
+}
+
+function createTimer(){
+    let time = document.createElement('input');
+    time.setAttribute('type', 'range');
+    time.setAttribute('max', '100');
+    time.setAttribute('min', '0');
+    time.setAttribute('step', '2');
+    return time;
+}
